47a4904f229eb95901e061adf78e3feab15463b6
JP19-03 <u202014511@upc.edu.pe>
2024-11-22 17:13:15 -0500

    chore: project setup

---
+++ src/main/java/com/smarthome/platform/upc/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,32 @@
+package com.smarthome.platform.upc.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI platformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("SmartHomeManagement Inc. API")
+                        .description("API for SmartHomeManagement Inc. services")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SmartHomeManagement Inc. Documentation")
+                        .url("https://www.smarthomemanagement.com"));
+
+
+        // Return OpenAPI configuration object with all the settings
+
+        return openApi;
+    }
+}
